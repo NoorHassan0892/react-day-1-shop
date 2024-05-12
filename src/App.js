ee2b4ae6eaaff7f26ce84af2fd9d73cb96d21e8e
Noor <noor.hassan0892@gmail.com>
2024-05-12 18:13:09 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cookies and Beyond</h1>
+      <h3>Where cookie maniacs gather</h3>
+      <img src="https://media.licdn.com/dms/image/C4E12AQEIFcEMGt4x5g/article-cover_image-shrink_720_1280/0/1591728324800?e=2147483647&v=beta&t=B5BzU1Zxe6TrM-UCS_SD68CsFzk-FB7rkHQdgYjNAjk"></img>
     </div>
   );
 }
